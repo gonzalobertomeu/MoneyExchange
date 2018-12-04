141d759be024bb4aa42a1bb2f85f2e1cb30f0879
Gonzalo Bertomeu <ggbertomeu@gmail.com>
2018-12-04 13:10:48 -0300

    Removiendo /dist

--- server/dist/index.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("./utils/express.factory");
-let server = express.init();
-server.listen(3000, () => {
-    console.log("Server's up");
-});
